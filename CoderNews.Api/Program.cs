443bc6cc7b9830e4253ea3498530c277873930bc
mohamed oueslati <eng.oueslati.mohamed@gmail.com>
2023-08-04 13:58:14 +0100

    Adding exception handling using problem details and error endpoint

--- CoderNews.Api/Program.cs
+++ CoderNews.Api/Program.cs
@@ -1,17 +1,20 @@
+using CoderNews.Api.Errors;
 using CoderNews.Application;
 using CoderNews.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
 builder.Services
            .AddApplication()
            .AddInfrastructure(builder.Configuration);
+builder.Services.AddControllers();
+builder.Services.AddSingleton<ProblemDetailsFactory , CoderNewsProblemDetailsFactory>();
+
 
 var app = builder.Build();
 
+app.UseExceptionHandler("/error");
 app.UseHttpsRedirection();
-
 app.MapControllers();
-
 app.Run();
